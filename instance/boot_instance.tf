ba18f74bd8b212c673b11852a40dd521fc95837d
Dinesh <deen.frndz@gmail.com>
2018-06-22 00:00:47 +0530

    terraform updated

--- instance/boot_instance.tf
+++ instance/boot_instance.tf
@@ -1,3 +1,8 @@
+variable "server_port" {
+  description = "port for project 1"
+  default     = 1234
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
@@ -10,4 +15,19 @@ resource "aws_instance" "Project1" {
   tags {
     Name = "awsec2"
   }
+}
+
+resource "aws_security_group" "Project1" {
+  name = "project1_security"
+
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.Project1.public_ip}"
 }
